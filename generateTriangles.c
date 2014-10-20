1063f0fdaab0931e857fe12e89804ab1056fc4be
Mitchell Karchemsky <mitch.karchemsky@gmail.com>
2014-10-20 15:31:29 -0700

    Made todos

---
+++ generateTriangles.c
@@ -0,0 +1,7 @@
+void main(){
+   //Open up Desktop Pattern Generator
+   //Generate new pattern based on time
+   //Save the new pattern
+   //Set the new pattern to be the desktop background
+   //Delete old patterns
+}
