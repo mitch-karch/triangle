b1e4477f7cb50ca37664a6654733abc307a2658e
Mitchell Karchemsky <mitch.karchemsky@gmail.com>
2014-10-27 15:51:39 -0700

    Added pyQt framework for openning and processing javascript website data

--- dataParser.py
+++ dataParser.py
@@ -1,4 +1,30 @@
-import urllib
+import sys
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+from PyQt4.QtWebKit import *
 
-generatorFile = urllib.urlopen("//webapp/webapp.html")
+#Scraper code from josechristian.com
+class Render(QWebPage):  
+    def __init__(self, url):  
+        self.app = QApplication(sys.argv)  
+        QWebPage.__init__(self)  
+        self.loadFinished.connect(self._loadFinished)  
+        self.mainFrame().load(QUrl(url))  
+        self.app.exec_()  
+   
+    def _loadFinished(self, result):  
+        self.frame = self.mainFrame()  
+        self.app.quit()
+ 
+def getHtml(str_url):
+    r_html = Render(str_url)  
+    html = r_html.frame.toHtml()
+ 
+    return html
+
+webGenServer = "http://pseudopri.me/triangle"
+
+html = getHtml(webGenServer)
+
+print str(html)
 
